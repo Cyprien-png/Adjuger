c4d1b3ae404aa244c45f16603c238b011d433092
MUMINOVIC Benjamin <Benjamin.MUMINOVIC@cpnv.ch>
2021-02-15 10:37:14 +0100

    Ajout du template et modèle MVC

---
+++ code/index.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+require "controler/controler.php";
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch ($action) {
+        case 'home':
+            home();
+            break;
+        case 'register':
+            register($_POST);
+            break;
+        case 'login':
+            login($_POST);
+            break;
+        case 'logout':
+            logout();
+            break;
+        default:
+            home();
+    }
+} else {
+    home();
+}
+
