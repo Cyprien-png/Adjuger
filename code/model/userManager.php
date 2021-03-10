0cacbcb461f1889ffcf4aa72709946dca907fbed
MUMINOVIC Benjamin <Benjamin.MUMINOVIC@cpnv.ch>
2021-03-10 12:17:28 +0100

    Added comments

--- code/model/userManager.php
+++ code/model/userManager.php
@@ -2,8 +2,10 @@
 /**
  * @author MUMINOVIC Benjamin
  * @creation-date 03.03.2021
- * @description Contains all the function to access or modify the json database
- * @modification
+ * @description Contains all the function to access or modify the json users file
+ * @modification 05.03.2021 : Started login functionality
+ *               10.03.2021 : Fixed the user appending in the json users file
+ *                            Completed the login functionality. Added comments
  *
  * */
 
@@ -23,27 +25,18 @@ function registerInDatabase($userData) {
  * @description Function used to append the data into the json file
  * @return bool True if the file writing succeeds or false if it fails
  * */
-// NOT TOTALLY WORKING
-// DUMBLY APPENDS DATA IN THE JSON FILE
+// /!\ Still doesn't verify if the user already exists in the database
 function insertData($data, $file) {
-    $currentData = file_get_contents($file);
-    $DcurrentData = json_decode($currentData, true);
+    $currentDataFile = file_get_contents($file);
+    $currentData = json_decode($currentDataFile, true);
 
-    $Dnewdata = json_decode($data, true);
+    $newData = json_decode($data, true);
+    array_push($CurrentData, $newData);
 
-    array_push($DcurrentData, $Dnewdata);
+    $finalData = json_encode($currentData, JSON_PRETTY_PRINT);
+    $success = file_put_contents($file, $finalData);
 
-    $DfinalData = json_encode($DcurrentData, JSON_PRETTY_PRINT);
-    $success =  file_put_contents($file, $DfinalData);
-
-
-
-//    $fileHandler = fopen($file, "a");
-//    $text = "$data \n";
-//    $success = fwrite($fileHandler, $text);
-//    fclose($fileHandler);
-
-    // fwrite doesn't return true on success, only false on fail
+    // file_put_contents() doesn't return true on success, only false on fail
     // this returns true if it doesn't fail
     if($success != false) {
         return true;
@@ -72,7 +65,10 @@ function prepareDataArray($userData) {
     return $dataArray;
 }
 
-
+/**
+ * @description Checks
+ * @return bool True if the account exists and the pseudo/e-mail matches with the password. False if it doesn't
+ * */
 function checkLogin($userData) {
     $check = false;
 
@@ -92,7 +88,11 @@ function checkLogin($userData) {
     return $check;
 }
 
-function searchUser($str,  $toMatch, $match) {
+/**
+ * @description Searches the json file to find an existing user
+ * @return bool True if the user exits. False if it doesn't
+ * */
+function searchUser($str, $toMatch, $match) {
     $result = false;
     $json = json_decode($str);
 
