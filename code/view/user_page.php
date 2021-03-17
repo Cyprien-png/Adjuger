eeefdd4e9ab97e82355803f52ed5b15be284ce2a
JAQUIER Cyprien <Cyprien.JAQUIER@cpnv.ch>
2021-03-17 12:00:20 +0100

    Starting account in page
    Made the account page displaying and responsive but it will probably changed because of features.

--- code/view/user_page.php
+++ code/view/user_page.php
@@ -6,16 +6,22 @@ ob_start();
 <head>
     <link href="view/css/style_lo.css" rel="stylesheet">
 </head>
-<div id="content-wrap">
+<br><h2>Votre compte</h2><br>
+<div class="container " id="content-wrap">
+    <div class="bordered center col-10 col-sm-8 col-md-8 col-lg-6">
+    
+    <div>
+        <img src="view/content/images/user_icon.png" style="height: 70px">
+    </div>
 
-    <img src="view/content/images/user_icon.png" style="height: 70px">
 
     <h5>Nom d'utilisateur : <?php echo $username; ?></h5>
 
     <p>E-Mail : <?php echo $email; ?></p>
 
     <a href="/index.php?action=logout" class="nav-item nav-link ">Déconnexion</a>
-
+    </div>
+</div>
 
 <?php
 $content = ob_get_clean();
