7f9e18e4f6b2991343002472f8e53344aa2b018e
MUMINOVIC Benjamin <Benjamin.MUMINOVIC@cpnv.ch>
2021-03-19 11:25:26 +0100

    Finished offer page

--- code/view/offer_add.php
+++ code/view/offer_add.php
@@ -4,21 +4,28 @@ ob_start();
 ?>
 
     <head>
-<!--        <link href="view/css/style_lo.css" rel="stylesheet">-->
+        <link rel="stylesheet" href="view/css/style_add-offer.css" >
     </head>
 
     <form id="frmAddOffer" method="post" action="index.php?action=login">
 
         <label for="txtOfferTitle">Titre : </label>
         <input type="text" id="OfferTitle" name="offerTitle" required>
-
-        <label for="txtPrice">Mot de passe* : </label>
+        <br><br>
+        <label for="txtPrice">Price : </label>
         <input type="number" id="txtPrice" name="offerPrice" required>
-
+        <br><br>
         <label for="txtDescription">Description : </label>
         <textarea id="txtDescription" name="offerDescription"></textarea>
+        <br><br>
+        <label for="fileImage">Image : </label>
+        <input type="file" id="fileImage" name="offerImage" placeholder="Image (PGN, JPG)" multiple="multiple6">
+        <br><br>
+        <label for="txtTags">Catégories : </label>
+        <input type="text" id="txtTags" name="offerTags">
+        <br><br>
+        <input type="submit" value="Poster l'annonce" id="btnSubmitSign">
 
-        <input type="submit" value="Se connecter" id="btnSubmitSign">
 
     </form>
 
